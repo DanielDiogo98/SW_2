309e2ab7d07cbfc3b8d6a81793a7354e2dadfb5e
Daniel DIogo <159474740+DanielDiogo98@users.noreply.github.com>
2026-02-19 10:21:21 -0300

    Add files via upload

---
+++ Aula_01/Exercicios/exe2.php
@@ -0,0 +1,10 @@
+<?php 
+    $n1 = 8;
+    $n2 = 7;
+    $n3 = 9;
+    $media = ($n1 + $n2 + $n3) /3;
+    echo "numero:1 => " .$n1. "<br>";
+    echo "numero:2 => " .$n2. "<br>" ;
+    echo "numero:3 => " .$n3. "<br>"  ;
+    echo "A média é: => " .$media ;
+?>
